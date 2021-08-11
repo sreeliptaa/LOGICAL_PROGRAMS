6fb0cd936dbe44304a56f9d36c1cd0b0aac3aa1c
sreelipta <sreelipta65@gmail.com>
2021-08-11 23:45:23 +0530

    Added: for measuring the time that elapses between

---
+++ src/com/logical/StopWatch.java
@@ -0,0 +1,45 @@
+package com.logical;
+
+import java.util.Scanner;
+
+/**
+ *  StopWatch that displays time elapsed
+ * @author Sreelipta
+ */
+public class StopWatch {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter any button to start");
+        s.next();
+        startTime();
+        System.out.println("enter any button to stop");
+        s.next();
+        stopTime();
+        elapsedTime();
+    }
+    /*
+     * static variables for storing starting and end time
+     */
+    static double start = 0, stop = 0;
+    /**
+     * Function to put start time and print it
+     */
+    static void startTime() {
+        start = System.currentTimeMillis();
+        System.out.println("started at " + start);
+    }
+    /**
+     * function to put stop time and print it
+     */
+    static void stopTime() {
+        stop = System.currentTimeMillis();
+        System.out.println("stopped at " + stop);
+    }
+
+    /**
+     * function to calculate elapsed time and print it
+     */
+    static void elapsedTime() {
+        System.out.println("elapsed time is " + (stop - start) / 1000);
+    }
+}
